786ce57eebcf5c4e16b54f173839411fdac29cbf
Ичи <ichi-perm@yandex.ru>
2022-09-07 12:41:13 +0500

    поставлено требование nodejs

--- app.vue
+++ app.vue
@@ -2,15 +2,7 @@
   <div>
     <SiteHeader/>
     <div>
-        <NuxtPage/>
+      <NuxtPage/>
     </div>
   </div>
 </template>
-<script>
-import SiteBar from "./components/SiteBar";
-import SiteFooter from "./components/SiteFooter";
-
-export default {
-  components: { SiteFooter, SiteBar }
-}
-</script>
